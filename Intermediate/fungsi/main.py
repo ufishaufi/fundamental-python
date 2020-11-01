7103053383133ac7bacc89e7b09f3c008d636d60
Shaufi <shaufiman@gmail.com>
2020-11-02 05:55:38 +0700

    update materi python fundamental

--- Intermediate/fungsi/main.py
+++ Intermediate/fungsi/main.py
@@ -53,7 +53,83 @@ def calculate_BMI(new_weight, new_height):
     new_bmi = new_weight/(pow(new_height, 2))
     return new_bmi
 
-weight = float(input('masukkan berat anda = '))
-height = float(input('masukkan tinggi anda = '))
-bmi = calculate_BMI(weight, height)
-print(bmi)
+# weight = float(input('masukkan berat anda = '))
+# height = float(input('masukkan tinggi anda = '))
+# bmi = calculate_BMI(weight, height)
+# print(bmi)
+
+# functional programming
+def add_ten(x):
+    return x + 10
+
+def twice(func, arg):
+    return func(func(arg))
+
+print(twice(add_ten, 10))
+
+# lambdas
+def square(x):
+    return x**2
+
+print(square(4))
+print((lambda x: x**2)(30))
+
+# map
+def add(x):
+    return x + 2
+
+newlist = [10, 20, 30, 40, 50]
+
+result = list(map(add, newlist))
+print(result)
+
+result = list(map(lambda x: x + 2, newlist))
+print(result)
+
+# filter
+newlist = [1, 3, 4, 5, 7, 2, 9, 11, 13]
+
+result =list(filter(lambda x: x % 2 == 0, newlist))
+print(result)
+
+# generator
+def function():
+    counter = 0
+    while counter < 5:
+        yield counter
+        counter += 1
+
+for x in function():
+    print(x)
+
+def even_numbers(x):
+
+    for i in range(x):
+        if i % 2 == 0:
+            yield i
+
+print(list(even_numbers(21)))
+
+# exercise
+def student_discount(price):
+    price = price - (price * 10) / 100
+    return  price
+
+def additional_discount(price):
+    price = price - (price * 5) / 100
+    return price
+
+selling_price = 100
+
+print(additional_discount(student_discount(selling_price)))
+
+result = (lambda x: x*(x + 5)**2)(5)
+print(result)
+
+def discount(price):
+    price = (int) (price - (price * 10) / 100)
+    return price
+
+product_price = [100, 200, 300, 400, 500]
+updated_price = map(discount, product_price)
+print(list(updated_price))
